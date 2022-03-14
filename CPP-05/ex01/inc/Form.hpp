c594fe7063817d7a09de6f7beae814872974d179
Hexacosichore <93528473+Hexacosichore@users.noreply.github.com>
2022-03-14 18:41:30 +0100

    lala

--- CPP-05/ex01/inc/Form.hpp
+++ CPP-05/ex01/inc/Form.hpp
@@ -1,15 +1,3 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   Form.hpp                                           :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: kbarbry <kbarbry@student.42lyon.fr>        +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/03/11 18:36:59 by kbarbry           #+#    #+#             */
-/*   Updated: 2022/03/11 18:40:28 by kbarbry          ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #pragma once
 
 #include <iostream>
@@ -24,5 +12,34 @@ class Form
 	
 	public:
 		Form( void );
-		Form( std::string name, int gradeSign, int gradeExecute );
+		Form( const std::string& name, int gradeSign, int gradeExecute );
+		Form( const Form &copyClass );
+		Form	&operator=( const Form &equalClass );
+		~Form( void );
+
+		const std::string&	getName( void ) const;
+		const bool			getSigned( void ) const;
+		const int			getGradeSign( void ) const;
+		const int			getGradeExecute( void ) const;
+
+		//Exceptions
+		class GradeTooHighException: std::exception
+		{
+			public:
+				virtual const char *what() const throw() {
+					return ("\033[0;31mGrade too high\033[0m, max grade is \033[0;34m1\033[0m.");
+				}
+		};
+		class GradeTooLowException: std::exception
+		{
+			public:
+				virtual const char *what() const throw() {
+					return ("\033[0;31mGrade too low\033[0m, min grade is \033[0;34m150\033[0m.");
+				}
+		};
 };
+
+std::ostream	&operator<<(std::ostream &ost, const Form &elem ) {
+	ost << "\033[0;31m" << elem.getName() << "\033[0m, signed : " << elem.getSigned() << ", grade for sign: " << elem.getGradeSign() << ", grade for execute : " << elem.getGradeExecute() << std::endl;
+	return ost;
+}
--- CPP-05/ex01/src/Form.cpp
+++ CPP-05/ex01/src/Form.cpp
@@ -1,12 +1,41 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   Form.cpp                                           :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: kbarbry <kbarbry@student.42lyon.fr>        +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/03/11 18:36:55 by kbarbry           #+#    #+#             */
-/*   Updated: 2022/03/11 18:36:57 by kbarbry          ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
+#include "Form.hpp"
 
+Form::Form( void ): _name("Nobody"), _gradeSign(150), _gradeExecute(150) {
+}
+
+Form::Form( const std::string& name, int gradeSign, int gradeExecute ): _name(name), _gradeSign(gradeSign), _gradeExecute(gradeExecute) {
+	if (gradeSign < 1 || gradeExecute < 1)
+		throw Form::GradeTooHighException();
+	else if (gradeSign > 150 || gradeExecute > 150)
+		throw Form::GradeTooLowException();
+}
+
+Form::Form( const Form &copyClass ) {
+	*this = copyClass;
+}
+
+Form	&Form::operator=( const Form &equalClass ) {
+	this->_name = equalClass._name;
+	this->_isSigned = equalClass._isSigned;
+
+	return *this;
+}
+
+Form::~Form( void ) {
+}
+
+const std::string&	Form::getName( void ) const {
+	return _name;
+}
+
+const bool			Form::getSigned( void ) const {
+	return _isSigned;
+}
+
+const int			Form::getGradeSign( void ) const {
+	return _gradeSign;
+}
+
+const int			Form::getGradeExecute( void ) const {
+	return _gradeExecute;
+}
